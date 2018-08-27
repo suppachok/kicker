a7b2f027ad062e762c8d4f968d0a6b76e4146118
suppachok <40672661+suppachok@users.noreply.github.com>
2018-08-27 13:58:39 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const config = require('./config');
 const { Message, OpType, Location } = require('../curve-thrift/line_types');
 //let exec = require('child_process').exec;
 
-const myBot = ['ใส่midตรงนี้'];
+#const myBot = ['ใส่midตรงนี้'];
 const banList = [];//Banned list
 var groupList = new Array();//Group list
 var vx = {};var midnornama,pesane,kickhim;var waitMsg = "no";//DO NOT CHANGE THIS
